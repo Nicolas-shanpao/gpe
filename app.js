b878ae652ad5bda83e5876abbffd74427c41096a
Administrator <375621354@qq.com>
2020-09-30 10:39:52 +0800

    content type

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.all('*', function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', 'http://bim.checc.com.cn');
+  res.header('Access-Control-Allow-Origin', '*');
   //Access-Control-Allow-Headers ,可根据浏览器的F12查看,把对应的粘贴在这里就行
   res.header('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With');
   res.header('Access-Control-Allow-Methods', '*');
