7a056842395c43dc3ff0bb0e44a0b97293846f41
Administrator <375621354@qq.com>
2020-10-22 13:34:41 +0800

    密码重置

--- routes/api2/users.js
+++ routes/api2/users.js
@@ -555,4 +555,50 @@ router.post('/updateUserPassword', auth, async (req, res) => {
     )
   }
 })
+
+/**
+ * @api {post} /api2/user/resetPassword 密码重置
+ * @apiDescription 密码重置
+ * @apiName resetPassword
+ * @apiGroup 用户
+ * @apiHeader authorization eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVmMjdjOTZhNTExNzdmNDIxY2ExNjI5NCIsImlhdCI6MTU5NjQ0Njc5MH0.ztinMsRDhVVKLh5GNbgngD7YsHOgj1OgCFYxz4V3MzM
+ * @apiSuccess {number} code 具体请看
+ * @apiSuccess {json} content
+ * @apiSuccess {string} message
+ * @apiSuccessExample {json} Success-Response:
+ * HTTP/1.1 200 OK
+ *  {
+ *  "code": 200,
+ *  "content": {
+ *      "roles": [
+ *          "admin"
+ *      ],
+ *      "isDeleted": 0,
+ *      "_id": "5f22910ccf2a793e64e74202",
+ *      "username": "g15",
+ *      "introduction": "这个人很懒，啥也没留......",
+ *      "avatar": "https://wpimg.wallstcn.com/f778738c-e4f8-4870-b634-56703b4acafe.gif",
+ *      "nikename": "g15",
+ *      "project": "g15",
+ *      "__v": 0
+ *  },
+ *  "message": "success"
+ * }
+ * @apiSampleRequest /api2/user/resetPassword
+ * @apiVersion 2.0.0
+ */
+// 密码重置
+router.post('/resetPassword', adminAuth, async (req, res) => {
+  let values = [require('bcrypt').hashSync('123123', 10), req.body.id];
+  let sql = "update user set password=? where id=?;"
+  let result = await query(sql, values);
+  if (result.affectedRows == 1) {
+    res.send({
+        code: 200,
+        content: '重置成功',
+        message: 'success'
+      }
+    )
+  }
+})
 module.exports = router;
