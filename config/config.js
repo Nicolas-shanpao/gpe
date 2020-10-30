26438542fecd30e0a129881846f5e874a5dce960
Administrator <375621354@qq.com>
2020-10-30 13:36:44 +0800

    手机号登录

--- config/config.js
+++ config/config.js
@@ -1,15 +1,8 @@
 const mysql = require("mysql")
 const config = {
   // 数据库配置(线上)
-  // database: {
-  //   DATABASE: "lzztest",
-  //   USERNAME: "rootbim",
-  //   PASSWORD: "RootBim2020!@#$",
-  //   PORT: "3306",
-  //   HOST: "localhost"
-  // },
   database: {
-    DATABASE: "BasicLibrary",
+    DATABASE: "lzztest",
     USERNAME: "rootbim",
     PASSWORD: "RootBim2020!@#$",
     PORT: "3306",
@@ -17,11 +10,11 @@ const config = {
   },
   // 数据库配置(本地)
   // database: {
-  //   DATABASE: "lzztest",
-  //   USERNAME: "liazz",
-  //   PASSWORD: "JUPyZP2wkgy0L22V",
+  //   DATABASE: "BasicLibrary",
+  //   USERNAME: "rootbim",
+  //   PASSWORD: "RootBim2020!@#$",
   //   PORT: "3306",
-  //   HOST: "0.0.0.0"
+  //   HOST: "localhost"
   // }
 };
 let pool = mysql.createPool({
