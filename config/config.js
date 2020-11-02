6d75b56e17efd93084d7f35bc072b017e9e65283
Administrator <375621354@qq.com>
2020-11-02 11:18:36 +0800

    线上

--- config/config.js
+++ config/config.js
@@ -1,21 +1,21 @@
 const mysql = require("mysql")
 const config = {
   // 数据库配置(线上)
-  // database: {
-  //   DATABASE: "lzztest",
-  //   USERNAME: "rootbim",
-  //   PASSWORD: "RootBim2020!@#$",
-  //   PORT: "3306",
-  //   HOST: "localhost"
-  // },
-  // 数据库配置(本地)
   database: {
     DATABASE: "lzztest",
-    USERNAME: "root",
+    USERNAME: "rootbim",
     PASSWORD: "RootBim2020!@#$",
     PORT: "3306",
     HOST: "localhost"
-  }
+  },
+  // 数据库配置(本地)
+  // database: {
+  //   DATABASE: "lzztest",
+  //   USERNAME: "root",
+  //   PASSWORD: "RootBim2020!@#$",
+  //   PORT: "3306",
+  //   HOST: "localhost"
+  // }
 };
 let pool = mysql.createPool({
   host: config.database.HOST,
